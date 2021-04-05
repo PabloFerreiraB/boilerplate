802fbe09caca0280f901ca884988732cb2ac8753
pablo <pablo_ferreira14@hotmail.com>
2021-04-05 14:08:44 -0300

    Configurando o TypeScript no NextJS

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+
+type Props = {
+  title: string
+}
+
+export default function Home({title = "React Avançado"}: Props) {
+  return (
+    <div className="container">
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container">
+        <h1 className="container">
+          {title}
+        </h1>
+      </main>
+
+      <footer className="container">
+       
+      </footer>
+    </div>
+  )
+}
