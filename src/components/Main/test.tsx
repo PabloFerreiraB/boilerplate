b727a6fc5ff42d42c2a3129383f6cebf9be6b5aa
pablo <pablo_ferreira14@hotmail.com>
2021-04-05 23:58:27 -0300

    Criando estilos no primeiro componente

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -12,4 +12,13 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#121212',
+      color: '#8899a6'
+    })
+  })
 })
