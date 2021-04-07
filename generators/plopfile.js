2a568fd16fe8249c17ca1ab35d9edf5b6a55ba15
pablo <pablo_ferreira14@hotmail.com>
2021-04-07 17:30:46 -0300

    Automatizando criação de arquivos com Plop

---
+++ generators/plopfile.js
@@ -0,0 +1,34 @@
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: 'Crie um componente',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Qual é o nome do seu componente?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/test.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      }
+    ]
+  })
+}
